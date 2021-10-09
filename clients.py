6b732bb25cb6bea31b4e4bb3b75fa45f971823f0
Brillianttyagi <deepanshubhai84@gmail.com>
2021-10-09 18:55:27 +0530

    updated client1 file

---
+++ clients.py
@@ -0,0 +1,19 @@
+import socketio
+
+sio = socketio.Client()
+
+@sio.event
+def connect():
+    print('connection established',sio.sid)
+
+@sio.event
+def message(data):
+    print('message received with ', data)
+    sio.emit('my response', {'response': 'my response'})
+
+@sio.event
+def disconnect():
+    print('disconnected from server')
+
+sio.connect('http://localhost:5000')
+sio.wait()
